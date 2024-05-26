8458524dc982fbedb7b9dbb7862e2f50afd11da9
Arun Fletcher <focalpear00@yahoo.co.uk>
2024-05-26 04:59:43 +0100

    Add actual question answering
    - Currently just uses an alert to correct the user

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 document.querySelector("footer").style.backgroundImage = "url(\"/img/60-lines.png\")"; // Doesn't work in CSS
 
-const gerOptions = ["ks_clear", "ks_clear_restricted", "ks_expect_stop", "ks_stop", "clear", "clear_100", "clear_60", "clear_40", "stop", "expect_clear", "expect_100", "expect_40_or_60", "expect_stop", "main", "distant", "repeater"]
-const ausOptions = ["clear", "clear_60", "clear_40", "stop", "expect_clear", "expect_60", "expect_40", "expect_stop", "shunt_clear", "shunt_admitted", "shunt_stop", "repeater_clear", "repeater_restriction", "repeater_stop", "repeater_stop_ack"]
+const gerOptions = ["repeater_expect_clear", "40_expect_clear", "ks_clear", "ks_clear_restricted", "ks_expect_stop", "ks_stop", "clear", "clear_100", "clear_60", "clear_40", "stop", "expect_clear", "expect_100", "expect_40_or_60", "expect_stop", "main", "distant", "repeater"]
+const ausOptions = ["clear", "clear_60", "clear_40", "stop", "expect_clear", "expect_60", "expect_40", "expect_stop", "shunt_clear", "shunt_admitted", "shunt_stop", "repeater_clear", "repeater_restricted", "repeater_stop", "repeater_stop_ack"]
 
 const regions = document.getElementById("regionSelector").children;
 const gerSelectedLine = document.getElementById("gerSelector");
@@ -9,6 +9,7 @@ const ausSelectedLine = document.getElementById("ausSelector");
 const inputBox = document.getElementById("userInput");
 const signal = document.getElementById("signal");
 let gerSelected = true;
+let currentSignal = "";
 
 ausSelectedLine.style.visibility = "hidden";
 
@@ -43,11 +44,11 @@ function setImage() {
     if (gerSelected) {
         randomNum = randomIntFromInterval(0, gerOptions.length-1);
         src = `https://aejae.github.io/Signalling-Training/img/signals/german/${gerOptions[randomNum]}.gif`;
-        console.log(gerOptions[randomNum]);
+        currentSignal = gerOptions[randomNum];
     } else {
         randomNum = randomIntFromInterval(0, ausOptions.length-1);
         src = `https://aejae.github.io/Signalling-Training/img/signals/austrian/${ausOptions[randomNum]}.gif`;
-        console.log(ausOptions[randomNum]);
+        currentSignal = ausOptions[randomNum];
     }
     signal.setAttribute("src", src);
 }
@@ -55,8 +56,21 @@ function setImage() {
 function userInput(e) {
     if (e.key === "Enter") {
         e.preventDefault();
-        if (inputBox.value.trim() !== "") {
-            console.log(inputBox.value);
+        const userAnswer = inputBox.value.trim().toLowerCase();
+        if (userAnswer !== "") {
+            let answer = currentSignal.split("_");
+            if (answer[0] === "ks") {
+                delete answer[0];
+            }
+            answer = answer.join(" ").trim().toLowerCase();
+            if (userAnswer === answer) {
+                console.log("Correct");
+            } else {
+                console.log("Answer:", answer.toUpperCase());
+                alert("Answer: " + answer.toUpperCase());
+            }
+            inputBox.value = "";
+            setImage();
         }
     }
 }
